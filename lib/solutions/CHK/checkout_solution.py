5ab25c300857167a03293cf5ac831fe571b778d3
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 18:34:34 +0000

    Sun Nov 05 18:34:34 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,17 +10,18 @@ item_prices = {"A": 50, "B": 30, "C": 20, "D": 15,
                "U": 40, "V": 50, "W": 20, "X": 90,
                "Y": 10, "Z": 50}
 
-item_counts = {"A": 0, "B": 0, "C": 0, "D": 0,
-               "E": 0, "F": 0, "G": 0, "H": 0,
-               "I": 0, "J": 0, "K": 0, "L": 0,
-               "M": 0, "N": 0, "O": 0, "P": 0,
-               "Q": 0, "R": 0, "S": 0, "T": 0,
-               "U": 0, "V": 0, "W": 0, "X": 0,
-               "Y": 0, "Z": 0}
+
 
 
 
 def checkout(skus):
+    item_counts = {"A": 0, "B": 0, "C": 0, "D": 0,
+                   "E": 0, "F": 0, "G": 0, "H": 0,
+                   "I": 0, "J": 0, "K": 0, "L": 0,
+                   "M": 0, "N": 0, "O": 0, "P": 0,
+                   "Q": 0, "R": 0, "S": 0, "T": 0,
+                   "U": 0, "V": 0, "W": 0, "X": 0,
+                   "Y": 0, "Z": 0}
     items = [*skus]
     if not items:
         return 0
@@ -38,12 +39,9 @@ def checkout(skus):
     #apply discounts
     # A discount
     fives = item_counts["A"] // 5
-    print(fives)
     discount += fives * 50
     remainder_a_count = item_counts["A"] - (fives * 5)
-    print(remainder_a_count)
     threes = remainder_a_count // 3
-    print(threes)
     discount += threes * 20
 
     # E discount before b
@@ -77,5 +75,6 @@ def checkout(skus):
 
 
     return total - discount
+
 
 
