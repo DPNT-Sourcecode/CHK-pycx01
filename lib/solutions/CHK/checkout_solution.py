9764b2e6e578ed6a332669b9c340232e968d47d0
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 17:52:04 +0000

    Sun Nov 05 17:52:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,19 +10,32 @@ def checkout(skus):
     items = [*skus]
     a_count = 0
     b_count = 0
+    e_count = 0
     total = 0
     discount = 0
     for item in items:
         if item == "A": a_count+=1
         elif item == "B": b_count+=1
+        elif item == "E":e_count += 1
         if item_prices.get(item):
             total += item_prices.get(item)
         else: return -1
 
     #apply discounts
-    if a_count>=3:
-        discount += (a_count // 3) * 20
-    if b_count>=2:
-        discount += (b_count // 2) * 15
+    # A discount
+    fives = a_count // 5
+    discount += fives * 50
+    remainder_a_count = a_count - (fives * 5)
+    threes = remainder_a_count // 3
+    discount += threes * 20
+
+
+    # B discount
+    discount += (b_count // 2) * 15
+
+    # E discount
+    free_bs = e_count // 2
+    discount = min(b_count, free_bs)*30
 
     return total - discount
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,6 +4,7 @@ from lib.solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout(self):
         assert checkout_solution.checkout("ABCD") == 115
+
     def test_checkout4a(self):
         assert checkout_solution.checkout("AAAABCD") == 245
 
@@ -18,3 +19,13 @@ class TestCheckout():
 
     def test_checkout_badandgood(self):
         assert checkout_solution.checkout("BCAa") == -1
+
+    def test_checkout2e2b(self):
+        assert checkout_solution.checkout("ABBBBCDEE") == 225
+
+    def test_checkout5a(self):
+        assert checkout_solution.checkout("AAAAABCD") == 245
+
+    def test_checkout8a(self):
+        assert checkout_solution.checkout("AAAAAAAABCD") == 375
+
