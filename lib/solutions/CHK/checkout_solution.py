3e983a356de6080957c075659b8d817c23f1f053
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 18:46:34 +0000

    Sun Nov 05 18:46:34 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -56,25 +56,45 @@ def checkout(skus):
     discount += (item_counts["F"] // 3) * 10
 
     # H discount
+    ten_hs = item_counts["H"] // 10
+    discount += ten_hs * 20
+    remainder_h_count = item_counts["H"] - (ten_hs * 10)
+    five_hs = remainder_h_count // 5
+    discount += five_hs * 5
 
     # K discount
+    discount += (item_counts["K"] // 2) * 10
 
     # N discount using Ms
+    free_ms = item_counts["N"] // 3
+    discount += min(item_counts["M"], free_ms)*15
 
     # P discount
+    discount += (item_counts["P"] // 5) * 50
 
     # R discount before Q
+    free_qs = item_counts["R"] // 3
+    remainder_q_count = item_counts["Q"] - min(item_counts["Q"], free_qs)
+    discount += min(item_counts["Q"], free_qs)*30
 
     # Q discount
+    discount += (remainder_q_count // 3) * 10
 
     # U discount
+    discount += (item_counts["U"] // 4) * 40
 
     # V discount
+    three_vs = item_counts["V"] // 3
+    discount += three_vs * 20
+    remainder_v_count = item_counts["V"] - (three_vs * 3)
+    two_vs = remainder_v_count // 2
+    discount += two_vs * 10
 
 
 
 
     return total - discount
+
 
 
 
