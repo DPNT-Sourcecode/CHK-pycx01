9c80ec66619b898408f1e28ecb66d9251e696f21
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 18:28:34 +0000

    Sun Nov 05 18:28:34 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,21 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-item_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+item_prices = {"A": 50, "B": 30, "C": 20, "D": 15,
+               "E": 40, "F": 10, "G": 20, "H": 10,
+               "I": 35, "J": 60, "K": 80, "L": 90,
+               "M": 15, "N": 40, "O": 10, "P": 50,
+               "Q": 30, "R": 50, "S": 30, "T": 20,
+               "U": 40, "V": 50, "W": 20, "X": 90,
+               "Y": 10, "Z": 50}
+
+item_counts = {"A": 0, "B": 0, "C": 0, "D": 0,
+               "E": 0, "F": 0, "G": 0, "H": 0,
+               "I": 0, "J": 0, "K": 0, "L": 0,
+               "M": 0, "N": 0, "O": 0, "P": 0,
+               "Q": 0, "R": 0, "S": 0, "T": 0,
+               "U": 0, "V": 0, "W": 0, "X": 0,
+               "Y": 0, "Z": 0}
 
 
 
@@ -26,6 +40,7 @@ def checkout(skus):
         else:
             print('here')
             return -1
+        item_counts.update(item, )
 
     #apply discounts
     # A discount
@@ -49,6 +64,24 @@ def checkout(skus):
     # F discount
     discount += (f_count // 3) * 10
 
+    # H discount
+
+    # K discount
+
+    # N discount using Ms
+
+    # P discount
+
+    # R discount before Q
+
+    # Q discount
+
+    # U discount
+
+    # V discount
+
+
 
 
     return total - discount
+
