2d422546e734e42520b212b74631f618d5532af5
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 17:23:15 +0000

    Sun Nov 05 17:23:15 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,10 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+item_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+
+
 def checkout(skus):
+    items = skus.split()
     raise NotImplementedError()
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -4,4 +4,3 @@ from lib.solutions.HLO import hello_solution
 class TestHello():
     def test_hello(self):
         assert hello_solution.hello("Tom") == "Hello, Tom!"
-
