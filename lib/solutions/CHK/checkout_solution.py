6ec2cd0d81fd079820f334b061c913a000e39bd5
sbx_user1051 <sbx_user1051@169.254.17.189>
2023-11-05 17:26:15 +0000

    Sun Nov 05 17:26:15 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,4 +8,18 @@ item_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
 
 def checkout(skus):
     items = skus.split()
+    a_count = 0
+    b_count = 0
+    total = 0
+    for item in items:
+        if item == "A": a_count+=1
+        elif item == "B": b_count+=1
+        total += item_prices.get(item)
+
+    #apply discounts
+    if a_count>=3:
+        
+    if b_count>=2:
+
     raise NotImplementedError()
+
